370995f62e926dac42f5116c7a6c21a910179af8
Daniel <danielmovsesyan.dev@gmail.com>
2024-11-16 00:53:08 +0400

    [FOO-69]: Initial pages for IDRAM fallback routes (#9)
    Co-authored-by: Daniel Movsesyan <danomov@Daniels-MacBook-Pro.local>

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
   }, [products]);
 
   return (
-    <NavigationMenu className="w-full sticky top-0 bg-background flex justify-between max-w-full border-b-2 border-zinc-100 px-4 md:px-16 py-4 md:py-6">
+    <NavigationMenu className="w-full sticky top-0 bg-background flex-none flex justify-between max-w-full border-b-2 border-zinc-100 px-4 md:px-16 py-4 md:py-6">
       <Link to="/">
         <img src={logo} alt="logo" className="h-8 md:h-auto" />
       </Link>
