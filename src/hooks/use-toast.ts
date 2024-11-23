f0eb5c2c8840af4dd379ac8e001d36c320e25504
Daniel Movsesyan <danomov@Daniels-MacBook-Pro.local>
2024-11-23 16:44:18 +0400

    [FOO-73]: [Frontend] [UI] Checkout page

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,11 +1,9 @@
 "use client";
 
-// Inspired by react-hot-toast library
-import * as React from "react";
-
+import { ReactNode, useEffect, useState } from "react";
 import type {
-  ToastActionElement,
   ToastProps,
+  ToastActionElement,
 } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
@@ -13,8 +11,8 @@ const TOAST_REMOVE_DELAY = 1000000;
 
 type ToasterToast = ToastProps & {
   id: string
-  title?: React.ReactNode
-  description?: React.ReactNode
+  title?: ReactNode
+  description?: ReactNode
   action?: ToastActionElement
 };
 
@@ -172,9 +170,9 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState);
+  const [state, setState] = useState<State>(memoryState);
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState);
     return () => {
       const index = listeners.indexOf(setState);
