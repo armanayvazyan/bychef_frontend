e4c4e35f67ff00c211f920146ece61d9d65f29f7
armanayvazyan <arman.ayvazyan@jetbrains.com>
2024-11-29 21:54:46 +0400

    Add Firebase foreground and background notifications
    Integrate foreground message handling in NotificationsWrapper to display notifications when the app is in use. Introduce a service worker script to manage background messages, ensuring notifications are shown even when the app is not active. This improves user engagement by providing timely updates through notifications.

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.24.0/firebase-messaging.js");
+import { messaging } from "@/firebase/firebaseConfig";
+
+messaging.onBackgroundMessage((payload) => {
+    console.log("Received background message:", payload);
+
+    // Customize the notification
+    const notificationTitle = payload.notification?.title || "Default Title";
+    const notificationOptions = {
+        body: payload.notification?.body || "Default body text",
+        icon: payload.notification?.icon || "/default-icon.png", // Adjust this path as needed
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- src/hocs/NotificationsWrapper.tsx
+++ src/hocs/NotificationsWrapper.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren, useEffect } from "react";
-import { getToken } from "firebase/messaging";
+import { getToken, onMessage } from "firebase/messaging";
 import { messaging } from "@/firebase/firebaseConfig";
 
 const NotificationsWrapper = ({ children }: PropsWithChildren) => {
@@ -9,11 +9,6 @@ const NotificationsWrapper = ({ children }: PropsWithChildren) => {
       const permission = await Notification.requestPermission();
 
       if (permission === "granted") {
-        const token2 =  getToken(messaging, {
-          vapidKey: "BLrEzq6TgEUUyNs4W5cb8r5MRRolr6nrLGKCmk3OPMiYIBOTnVuuvF0eiP5w6A5iYpZN_LISvkuOIOpsMckHNiI",
-        });
-        console.log("Token generated : ", token2);
-
         const token =  await getToken(messaging, {
           vapidKey: "BLrEzq6TgEUUyNs4W5cb8r5MRRolr6nrLGKCmk3OPMiYIBOTnVuuvF0eiP5w6A5iYpZN_LISvkuOIOpsMckHNiI",
         });
@@ -23,6 +18,17 @@ const NotificationsWrapper = ({ children }: PropsWithChildren) => {
       }
     };
     requestNotificationPermission();
+
+    onMessage(messaging, (payload) => {
+      console.log("Message received in foreground:", payload);
+
+      if (payload.notification) {
+        const { title, body, icon } = payload.notification;
+        if (typeof title === "string") {
+          new Notification(title, { body, icon });
+        }
+      }
+    });
   }, []);
 
   return (
