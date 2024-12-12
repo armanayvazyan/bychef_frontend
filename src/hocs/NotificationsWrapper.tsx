8765fda3045aa914015393826dfe40de89953b3c
armanayvazyan <arman.ayvazyan@jetbrains.com>
2024-12-13 00:46:12 +0400

    Add alert to display registration token in NotificationsWrapper
    An alert was added to show the device registration token after fetching it and copying it to the clipboard. This helps users verify the token more easily during debugging or development.

--- src/hocs/NotificationsWrapper.tsx
+++ src/hocs/NotificationsWrapper.tsx
@@ -44,7 +44,7 @@ const NotificationsWrapper = ({ children }: PropsWithChildren) => {
             // @ts-ignore
             await fetch("http://192.168.10.12:8081/notifications/register-device", requestOptions);
             await navigator.clipboard.writeText(token);
-            alert(token);
+            window.alert(token);
           } else {
             console.warn("No registration token available. Request permission to generate one.");
           }
