5a1e704c0793d3e91f60766175abb323b7ca3c5c
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-01-07 23:06:50 +0400

    improve translation logic

--- src/helpers/getDataByLocale.ts
+++ src/helpers/getDataByLocale.ts
@@ -1,7 +1,12 @@
 import { ILocalizedData } from "@/types";
 
 const getDataByLocale = (collection: ILocalizedData[], locale: string) => {
-  return collection.find((item: ILocalizedData) => item.languageCode === locale)?.value;
+  return collection.find((item: ILocalizedData) => item.languageCode === locale.split("-")[0])?.value;
+};
+
+export const getDataStringByLocale = (obj: object, key: string, locale: string) => {
+  const keyLocale = locale.charAt(0).toUpperCase() + locale.slice(1);
+  return obj[(key + keyLocale) as keyof typeof obj];
 };
 
 export default getDataByLocale;
