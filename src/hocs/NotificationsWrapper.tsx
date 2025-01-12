93b1f0660ce6b6dd78c0964c18cc574c2eada215
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-01-12 15:23:49 +0400

    fix firebase notif config

--- src/hocs/NotificationsWrapper.tsx
+++ src/hocs/NotificationsWrapper.tsx
@@ -1,18 +1,19 @@
 import { PropsWithChildren, useEffect } from "react";
-// import { getToken, onMessage } from "firebase/messaging";
-// import { messaging } from "@/firebase/firebase/Config";
-// import { FIREBASE_VAPID_KEY } from "@/configs/constants";
+import { getToken, onMessage } from "firebase/messaging";
+import { FIREBASE_VAPID_KEY } from "@/configs/constants";
+import { messaging } from "@/firebase/firebaseConfig";
 
 const NotificationsWrapper = ({ children }: PropsWithChildren) => {
 
-  // useEffect(() => {
-  //   onMessage(messaging, (payload) => {
-  //     console.log("Received foreground message ", payload);
-  //     const title = payload.notification?.title ??  "";
-  //     const body = payload.notification?.body ?? "";
-  //     new Notification(title, { body: body });
-  //   });
-  // }, []);
+  useEffect(() => {
+    onMessage(messaging, (payload) => {
+      console.log("Received foreground message ", payload);
+      const title = payload.notification?.title ??  "";
+      const body = payload.notification?.body ?? "";
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      new Notification(title, { body: body });
+    });
+  }, []);
 
   useEffect(() => {
     const requestNotificationPermission = async () => {
@@ -20,18 +21,19 @@ const NotificationsWrapper = ({ children }: PropsWithChildren) => {
         const permission = await Notification.requestPermission();
 
         if (permission === "granted") {
-          // const token = await getToken(messaging, {
-          //   vapidKey: FIREBASE_VAPID_KEY,
-          // });
+          const token = await getToken(messaging, {
+            vapidKey: FIREBASE_VAPID_KEY,
+          });
 
-          // if (token) {
-          //   console.log("Token generated:", token);
-          //   // Temp solution to test mobile notifications
-          //   await navigator.clipboard.writeText(token);
-          //   alert("Copied to clipboard");
-          // } else {
-          //   console.warn("No registration token available. Request permission to generate one.");
-          // }
+          if (token) {
+            console.log("Token generated:", token);
+            // Temp solution to test mobile notifications
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            await navigator.clipboard.writeText(token);
+            alert("Copied to clipboard");
+          } else {
+            console.warn("No registration token available. Request permission to generate one.");
+          }
         } else if (permission === "denied") {
           alert("You denied notification permissions.");
         } else {
