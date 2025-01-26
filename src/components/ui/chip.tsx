c1c1a6de23f4e4861962a3e74443e03efb836549
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-01-27 00:30:20 +0400

    [FOO-143]: Chef details page

--- src/components/ui/chip.tsx
+++ src/components/ui/chip.tsx
@@ -1,6 +1,6 @@
 const Chip = ({ label }: { label: string }) => {
   return (
-    <span className="px-3 py-2 bg-zinc-100 w-max text-zinc-500 rounded-md text-sm">
+    <span className="px-3 py-2 bg-zinc-100 w-max text-zinc-500 rounded-md text-sm whitespace-nowrap">
       {label}
     </span>
   );
