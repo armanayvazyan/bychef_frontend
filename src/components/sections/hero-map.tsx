d04c4d7c1b372bdc8b043541ffd77c56294e8312
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-01-28 18:43:52 +0400

    add hero map

--- src/components/sections/hero-map.tsx
+++ src/components/sections/hero-map.tsx
@@ -1,9 +1,7 @@
 import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import Input from "@/components/ui/input";
 import Button from "@/components/ui/button";
-import { useToast } from "@/hooks/use-toast";
 import { useTranslation } from "react-i18next";
-import useFetchData from "@/hooks/use-fetch-data";
 import Map from "@/components/sections/map";
 
 interface FormElements extends HTMLFormControlsCollection {
@@ -22,9 +20,6 @@ const HeroTemp = () => {
     address: "",
   });
 
-  const { fetchData } = useFetchData("waitlist");
-  const { toast } = useToast();
-
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInfo(info => ({
       ...info,
@@ -34,28 +29,7 @@ const HeroTemp = () => {
 
   const handleSubmitData = useCallback(async (e: FormEvent<UsernameFormElement>): Promise<void> => {
     e.preventDefault();
-
-    // const response = await fetchData({
-    //   method: "POST",
-    //   bodyParams: {
-    //     fullName: e.currentTarget.elements.nameInput.value,
-    //     phoneNumber: e.currentTarget.elements.phoneInput.value,
-    //   }
-    // });
-    //
-    // if (!response?.error) {
-    //   toast({
-    //     title: "Գրանցումը կատարված է",
-    //     description: "Սպասե՜ք նորություններին",
-    //   });
-    // } else {
-    //   toast({
-    //     variant: "destructive",
-    //     title: "Oops!",
-    //     description: response.error,
-    //   });
-    // }
-  }, [fetchData, toast]);
+  }, []);
 
   return (
     <section className="hero relative">
@@ -63,8 +37,8 @@ const HeroTemp = () => {
         <img src="https://static.bychef.am/landing/hero.webp" alt="hero image" className="object-cover w-full h-[718px]"/>
         <div className="absolute w-full max-w-[560px] px-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="bg-white flex flex-col items-center pl-8 p-6 gap-3">
-            <h1 className="text-2xl font-extrabold">{t("subscribe.title")}</h1>
-            <p className="text-center text-zinc-600">{t("subscribe.details")}</p>
+            <h1 className="text-2xl font-extrabold">Ընտրեք ձեր առաքման հասցեն</h1>
+            <p className="text-center text-zinc-600">Գրեք այն հասցեն, որտեղ կուզենաք ոռպեսզի առաքեն ձեր պատվերը</p>
             <div className="flex gap-2 w-full">
               <Input
                 type="text"
@@ -72,11 +46,11 @@ const HeroTemp = () => {
                 id="addressInput"
                 value={info.address}
                 onChange={handleChange}
-                placeholder={t("subscribe.phone-input")}
+                placeholder="Մուտքագրեք հասցեն"
               />
               <Button type="submit" disabled={!info.address}>{t("subscribe.button")}</Button>
             </div>
-            <Map trigger={<Button variant="ghost2">Open map</Button>} />
+            <Map trigger={<Button variant="ghost2">Գտնել քարտեզով այստեղ</Button>} />
           </div>
         </div>
       </form>
--- src/components/sections/map.tsx
+++ src/components/sections/map.tsx
@@ -1,12 +1,97 @@
-import { YMaps, Map as Mapp } from "@pbe/react-yandex-maps";
+import { ReactNode, useRef, useState } from "react";
+import { MapPin, X } from "lucide-react";
+import Input from "@/components/ui/input";
+import Button from "@/components/ui/button";
+import { Map as YMap, useYMaps, YMaps } from "@pbe/react-yandex-maps";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
-const Map = () => {
+const Map = ({ trigger }: { trigger: ReactNode }) => {
+  const ymaps = useYMaps(["Map"]);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [inputValue, setInputValue] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleSearchAddress = async () => {
+    const key = import.meta.env.VITE_YMAP_KEY as string;
+    const url = new URL("https://suggest-maps.yandex.ru/v1/suggest");
+    url.searchParams.set("apikey", key);
+    url.searchParams.set("text", inputValue);
+    url.searchParams.set("results", "5");
+    url.searchParams.set("lang", "en_AM");
+    url.searchParams.set("ll", "44.491567,40.153759");
+    url.searchParams.set("spn", "0.473785400390625,0.7407931148263032");
+
+    const res = await fetch(url);
+
+    console.log(res);
+  };
+
+  const getGeoLocation = async (e: any) => {
+    if (ymaps) {
+      const coord = e.get("target").getCenter() as number[];
+      const response = await ymaps.geocode(coord);
+      // @ts-expect-error remove error
+      const currentAddress = response.geoObjects.get(0).getAddressLine() as string;
+
+      if (currentAddress) {
+        setAddress(currentAddress);
+      }
+    }
+  };
+
   return (
-    <YMaps query={{ lang: "en_US" }}>
-      <div>My awesome application with maps!</div>
-      <Mapp defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+    <Dialog>
+      <DialogTrigger>{trigger}</DialogTrigger>
+      <DialogContent
+        className="rounded-lg"
+        closeIcon={
+          <X size={24}/>
+        }
+      >
+        <DialogTitle>Մուտքագրեք Ձեր հասցեն</DialogTitle>
+        <div>
+          {address}
+          <div className="flex w-full gap-2 items-center mb-6">
+            <Input
+              type="text"
+              ref={inputRef}
+              value={inputValue}
+              placeholder="Մուտքագրեք հասցեն"
+              className="flex-1"
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+            />
+            <Button type="submit" onClick={handleSearchAddress}>Փնտրել</Button>
+          </div>
+          <div className="relative w-full h-[500px]">
+            <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-30">
+              <MapPin width={30} height={30} color="red"/>
+            </div>
+            <YMap
+              defaultState={{
+                center: [40.188508, 44.516095],
+                zoom: 17
+              }}
+              className="w-full h-full"
+              onBoundsChange={getGeoLocation}
+              modules={["geolocation", "geocode"]}
+            />
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const YMapWrappedMap = ({ trigger }: { trigger: ReactNode }) => {
+  return (
+    <YMaps query={{ lang: "en_US", apikey: import.meta.env.VITE_YMAP_KEY }}>
+      <Map trigger={trigger} />
     </YMaps>
   );
 };
 
-export default Map;
+export default YMapWrappedMap;
