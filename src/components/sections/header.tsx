f49f182a017b3a4c8ca12302ce2a02d739884278
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-02-11 00:05:38 +0400

    change header height, fix header spacing

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -11,34 +11,38 @@ const Header = () => {
   const sessionLocation = useLiveQuery(() => db.location.toArray());
 
   return (
-    <NavigationMenu className="w-full sticky top-0 bg-background flex-none flex-col md:flex max-w-full gap-5 border-b-2 border-zinc-100 px-4 md:px-16 py-4 md:py-6">
+    <NavigationMenu className="w-full sticky top-0 bg-background flex-none flex-col md:flex max-w-full gap-5 border-b-2 border-zinc-100 px-4 md:px-16 py-2">
       <div className="w-full flex justify-between items-center gap-4">
         <Link to={sessionLocation?.[0] ? "/explore" : "/"} className="shrink-0">
           <img src={logo} alt="logo" className="h-8 md:h-auto"/>
         </Link>
         {!!sessionLocation?.length && (
-          <Map
-            trigger={
-              <div className="hidden gap-2 md:flex items-center">
-                <MapPin className="shrink-0" />
-                <p className="text-base font-bold text-primary underline">{sessionLocation[0].address}</p>
-              </div>
-            }
-          />
+          <span className="hidden md:inline">
+            <Map
+              trigger={
+                <div className="hidden gap-2 md:flex items-center">
+                  <MapPin className="shrink-0" />
+                  <p className="text-base font-bold text-primary underline">{sessionLocation[0].address}</p>
+                </div>
+              }
+            />
+          </span>
         )}
         <UserCart/>
       </div>
       {!!sessionLocation?.length && (
-        <Map
-          trigger={
-            <div className="flex gap-2 md:hidden items-center">
-              <span className="flex-1">
-                <MapPin />
-              </span>
-              <p className="text-base font-bold text-primary underline">{sessionLocation[0].address}</p>
-            </div>
-          }
-        />
+        <span className="md:hidden">
+          <Map
+            trigger={
+              <div className="flex gap-2 items-center">
+                <span className="flex-1">
+                  <MapPin />
+                </span>
+                <p className="text-base font-bold text-primary underline">{sessionLocation[0].address}</p>
+              </div>
+            }
+          />
+        </span>
       )}
     </NavigationMenu>
   );
