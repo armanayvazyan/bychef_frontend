619576143712db34d629a6a4637ee5fcf4d37b21
Daniel Movsesyan <danielmovsesyan.dev@gmail.com>
2025-03-08 14:13:30 +0400

    add copy to clipboard action translations

--- src/components/sections/copy-id.tsx
+++ src/components/sections/copy-id.tsx
@@ -1,19 +1,21 @@
 import { Copy } from "lucide-react";
 import { useCallback } from "react";
 import { useToast } from "@/hooks/use-toast";
+import { useTranslation } from "react-i18next";
 
 const CopyId = ({ id }: { id?: string }) => {
   const { toast } = useToast();
+  const { t } = useTranslation("translation", { keyPrefix: "generic" });
 
   const handleCopyId = useCallback(async () => {
     if (id) {
       await navigator.clipboard.writeText(id);
 
       toast({
-        title: "Order id copied to clipboard!",
+        title: t("copied"),
       });
     }
-  }, [id, toast]);
+  }, [id, t, toast]);
 
   return (
     <div className="flex gap-2 items-center cursor-pointer" onClick={handleCopyId}>
