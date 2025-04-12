1339e94bf54ea658496196cc8684db9bd626215d
Test Name <Arman.Ayvazyan@jetbrains.com>
2025-04-12 20:13:36 +0400

    Fix delivery price condition and add new error message translations
    Updated the condition for displaying the delivery price to properly handle undefined values in the order checkout component. Added a new "distance-not-supported" error message to the Russian, Armenian, and English localization files.

--- src/components/sections/order-checkout.tsx
+++ src/components/sections/order-checkout.tsx
@@ -300,12 +300,12 @@ const OrderCheckout = () => {
         {!!cartItems.length && (
           <div className="flex justify-between text-base font-bold text-zinc-800 mb-8">
             <p>{t("total")}</p>
-            {deliveryInfoResponse.data?.result?.deliveryPrice && <p>{formatPrice(orderTotalPrice)} ֏</p>}
+            {deliveryInfoResponse.data?.result?.deliveryPrice !== undefined && <p>{formatPrice(orderTotalPrice)} ֏</p>}
             {deliveryInfoResponse.isLoading && <Skeleton className="w-[100px]" />}
           </div>
         )}
         <Button type="submit" disabled={submissionDisabled}>
-          {deliveryInfoResponse.data?.result?.deliveryPrice && t("pay", { amount: formatPrice(orderTotalPrice) })}
+          {deliveryInfoResponse.data?.result?.deliveryPrice !== undefined && t("pay", { amount: formatPrice(orderTotalPrice) })}
         </Button>
       </div>
     </Form>
